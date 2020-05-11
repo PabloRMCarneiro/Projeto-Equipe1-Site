4828c811dee134bf2e950571365fb1121698f641
Pablo <pablormc3dd3@gmail.com>
2020-05-11 16:31:02 -0300

    cadastramento de produtos

--- inicio_react/src/routes.js
+++ inicio_react/src/routes.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Registro from './pages/Registro/Registro'
 import Estoque from './pages/Estoque/Estoque'
+import Novos_produtos from './pages/Novos_produtos/Novos_produtos'
 export default function Routes(){
     return(
         <BrowserRouter>
@@ -11,8 +12,9 @@ export default function Routes(){
                 <Route path="/login"  exact component = {Login}/>
                 <Route path="/registro"  exact component = {Registro}/>
                 <Route path="/estoque"  exact component = {Estoque}/>
+                <Route path="/novos_produtos"  exact component = {Novos_produtos}/>
             </Switch>
         </BrowserRouter>
     )
 }
-//<Route path="/estoque"  exact component = {Estoque}/>
+
